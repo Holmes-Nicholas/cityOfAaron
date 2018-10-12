83d8be2ffe50a8dcfe837506ee242bd029ed4405
js-goff <38774971+js-goff@users.noreply.github.com>
2018-10-11 18:12:13 -0600

    add hello message

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -16,6 +16,7 @@ public class CityOfAaron {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Welcome to the City Of Aaron");
     }
     
 }
