40dc64054098e6ea48fbfda5c92c7ecbf7e75e1c
Nicholas Holmes <>
2018-10-12 13:56:13 -0400

    10/12/18 Moved Getters after all Setters

--- cityOfAaron/src/model/CropData.java
+++ cityOfAaron/src/model/CropData.java
@@ -23,63 +23,65 @@ public class CropData {
     private int wheatForFood;
     private int acresPlanted;
     
-    //getters and setters for instance fields
+    //setters for instance fields
     public void setYear(int _year){
         year = _year;
-    }
-    public int getYear(){
-        return year;
     }
     public void setPopulation(int _population){
         population = _population;
     }
-    public int getPopulation(){
-        return population;
-    }
     public void setAcresOwned(int _acresOwned){
         acresOwned = _acresOwned;
-    }
-    public int getacresOwned(){
-        return acresOwned;
     }
     public void setCropYield(int _cropYield){
         cropYield = _cropYield;
     }
-    public int getCropYield(){
-        return cropYield;
-    }
     public void setWheatInStore(int _wheatInStore){
         wheatInStore = _wheatInStore;
-    }
-    public int getWheatInStore(){
-        return wheatInStore;
     }
     public void setNumberWhoDied(int _numberWhoDied){
         numberWhoDied = _numberWhoDied;
     }
-    public int getNumberWhoDied(){
-        return numberWhoDied;
-    }
     public void setNewPeople(int _newPeople){
         newPeople = _newPeople;
-    }
-    public int getNewPeople(){
-        return newPeople;
     }
     public void setHarvest(int _harvest){
         harvest = _harvest;
     }
-    public int getHarvest(){
-        return harvest;
-    }
     public void setWheatForFood(int _wheatForFood){
         wheatForFood = _wheatForFood;
     }
-    public int getWheatForFood(){
-        return wheatForFood;
-    }
     public void setAcresPlanted(int _acresPlanted){
         acresPlanted = _acresPlanted;
+    }
+    
+    //getters for instance fields
+    public int getYear(){
+        return year;
+    }
+    public int getPopulation(){
+        return population;
+    }
+    public int getAcresOwned(){
+        return acresOwned;
+    }
+    public int getCropYield(){
+        return cropYield;
+    }
+    public int getWheatInStore(){
+        return wheatInStore;
+    }
+    public int getNumberWhoDied(){
+        return numberWhoDied;
+    }
+    public int getNewPeople(){
+        return newPeople;
+    }
+    public int getHarvest(){
+        return harvest;
+    }
+    public int getWheatForFood(){
+        return wheatForFood;
     }
     public int getAcresPlanted(){
         return acresPlanted;
