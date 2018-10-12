02725224b3f82a2f82f41a68782b870708ede0a2
js-goff <38774971+js-goff@users.noreply.github.com>
2018-10-11 19:13:09 -0600

    Model and Player Java.

---
+++ cityOfAaron/src/model/Player.java
@@ -0,0 +1,16 @@
+/*
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Fall 2018
+ * Team members: Nicholas Holmes, David Nielson, Jared Goff
+ */
+package model;
+
+/**
+ *
+ * @author jgoff
+ */
+public class Player 
+{
+    
+}
