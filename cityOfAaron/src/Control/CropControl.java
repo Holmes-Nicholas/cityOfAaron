4906bc4cdd7184cf1a6b53ffde821bbf4bfc28ac
js-goff <38774971+js-goff@users.noreply.github.com>
2018-10-27 20:15:38 -0600

    10.27.18 Created PlantCrop method in CropControl. JG

--- cityOfAaron/src/Control/CropControl.java
+++ cityOfAaron/src/Control/CropControl.java
@@ -77,5 +77,44 @@ public class CropControl {
         //return wheatInStore
         return _wheatInStore;
     }
+    /**
+ *
+ * @author jgoff
+ */
+
+/**
+* The plantCrops method
+* Purpose: Set aside wheat to plant next year.
+* @param acresPlanted to a CropData cropData object.
+* @param cropData
+* @return bushels of wheat in store 
+* Pre-conditions: bushels of wheat to set aside must be >= zero
+* and >= bushels of wheat in store
+**/
+    
+    public static int plantCrops(int acresPlanted, CropData cropData){
+        //if acresPlanted <= 0, return -1
+        if (acresPlanted <= 0)
+            return -1;
+    
+        //if population <= acresPlanted*.1, return -1
+        int peoplePerAcre = cropData.getPopulation();
+        if(peoplePerAcre <= acresPlanted * .1)
+            return -1;
+        
+        //if wheatInStore < acresPlanted/2, return -1
+        int WheatInStore = cropData.getWheatInStore();
+        if (WheatInStore < acresPlanted/2)
+            return -1;
+               
+        
+        //if acresOwned is <= acresPlanted return -1
+        int acresOwned = cropData.getAcresOwned();                
+        if (acresOwned <= acresPlanted)
+            return -1;
+               
+        //return acresPlanted
+        return acresPlanted;  
+    } 
 
 }
