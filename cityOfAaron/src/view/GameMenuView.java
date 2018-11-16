869ebdd083dca50d5bb1a4130fad27f67aee2998
js-goff <38774971+js-goff@users.noreply.github.com>
2018-11-16 16:43:50 -0700

    11.16.18 Updated GameMenuView class to inherit from MenuView. Changed GameMenuView method to use 'super' to call from the base constructor. Used @Override to tell doAction method to override method in ViewInterface. Removed private string theMenu, private int max, and new scanner object. Removed code for displayMenuView and getMenuOption methods. Updated code in HelpMenuView class to call displayMenu method instead of displayMenuView.

--- cityOfAaron/src/view/GameMenuView.java
+++ cityOfAaron/src/view/GameMenuView.java
@@ -1,7 +1,7 @@
 // The GameMenuView class - part of the view layer
 // Object of this class manages the main menu
 // Author:  Jared Goff
-// Date last modified: Nov 7 2018
+// Date last modified: Nov 16 2018
 //-------------------------------------------------------------
 
 package view;
@@ -10,35 +10,13 @@ import Control.GameControl;
 import java.util.Scanner;
 import cityofaaron.CityOfAaron;
 
-public class GameMenuView
+/**
+ *
+ * @author Jared Goff
+ */
+    //Added extends MenuView so it will inherit from MenuView.
+public class GameMenuView extends MenuView
 {
-  Scanner keyboard = new Scanner(System.in);
-  
-  private String theMenu;
-  private int max;
-  
-  //The GameMenuView Method
-  //Purpose: Open GameMethodView
-  //Parameters: none
-  //Parameters: none
-  
-  
-   public void displayMenuView()
-    {
-        int menuOption;
-        do
-        {
-            // Display the menu
-            System.out.println(theMenu);
-            
-            // Prompt the user and get the user’s input
-            menuOption=getMenuOption();
-            
-            // Perform the desired action
-             doAction(menuOption);
-        } while (menuOption != max);
-
-    }
     //The Game MenuView constructor
     //purpose: Intialize the game menu
     //Parameters: none
@@ -46,7 +24,7 @@ public class GameMenuView
 
   public GameMenuView()
   {
-     theMenu = "\n" +
+     super("\n" + //'super' calls the base class construction Jared
           "**********************************\n" +
           "* CITY OF AARON: MAIN GAME MENU  *\n" +
           "**********************************\n" +
@@ -54,44 +32,18 @@ public class GameMenuView
           " 2 - viewList\n" +
           " 3 - moveToNewLocation\n" +
           " 4 - manageCrops\n" +
-          " 5 - Quit\n";
+          " 5 - Quit\n",
         
-     max = 5;
+     5);
   }
-  
-    // The getMenuOption method  
-    // Purpose: gets the user's input
-    // Parameters: none
-    // Returns: integer - the option selected
-    // ===================================       
-    public int getMenuOption()
-    {
-       // declare a variable to hold user’s input
-       int userInput = 0;
-       final int MAX = 5;
-       Scanner keyboard = new Scanner(System.in);
 
-       // begin loop
-       do
-       {
-            // get user input from the keyboard
-            userInput = keyboard.nextInt();
-            // if it is not a valid value, output an error message
-            if(userInput < 1 || userInput > MAX)
-                System.out.println("Error: you must select 1, 2, 3, 4, or 5");
-            // loop back to the top of the loop if input was not valid
-        // end loop
-        } while (userInput < 1 || userInput > MAX);
-       
-       return userInput;
-    }
-    
     // The doAction method
     // Purpose: performs the selected action
     // Parameters: none
     // Returns: none
-    // ===================================       
-    public void doAction(int option)
+    // =================================== 
+    //@Override overides the method in the ViewInterface class.
+    @Override public void doAction(int option)
     {
       switch(option)
       {
--- cityOfAaron/src/view/MainMenuView.java
+++ cityOfAaron/src/view/MainMenuView.java
@@ -94,7 +94,7 @@ public class MainMenuView extends MenuView
 
     // Display the Game menu
     GameMenuView gmv = new GameMenuView();
-    gmv.displayMenuView();
+    gmv.displayMenu();
   
     }
     
